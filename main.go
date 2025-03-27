d10b074678698e261f2cd011615acff423d4a560
Adam <sector-f@users.noreply.github.com>
2025-03-26 20:19:15 -0400

    Change output format

--- main.go
+++ main.go
@@ -5,19 +5,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
+	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const upcomingURL = "https://www.coasttocoastam.com/shows/upcoming/"
+const (
+	upcomingURL = "https://www.coasttocoastam.com/shows/upcoming/"
+	timeFormat  = "Monday, January 2, 2006"
+)
+
+type Output struct {
+	Shows []Show `json:"shows,omitempty"`
+}
 
 type Show struct {
-	Title      string   `json:"title,omitempty"`
-	Date       string   `json:"date,omitempty"`
-	Host       string   `json:"host,omitempty"`
-	Guests     []string `json:"guests,omitempty"`
-	FirstHalf  string   `json:"first_half,omitempty"`
-	SecondHalf string   `json:"second_half,omitempty"`
+	Title      string `json:"title,omitempty"`
+	Date       string `json:"date,omitempty"`
+	Host       string `json:"host,omitempty"`
+	Guests     string `json:"guests,omitempty"`
+	FirstHalf  string `json:"first_half,omitempty"`
+	SecondHalf string `json:"second_half,omitempty"`
 }
 
 func main() {
@@ -32,26 +41,30 @@ func main() {
 		log.Fatal(err)
 	}
 
-	shows := []Show{}
+	out := Output{}
 	doc.Find(".feed-cards .coast-feed-item").Each(func(i int, s *goquery.Selection) {
 		show := Show{
 			Title:      s.Find(".component-heading").Text(),
-			Date:       s.Find("time").First().Text(),
 			Host:       s.Find(".coast-linked-host .linked-value").Text(),
 			FirstHalf:  s.Find(".item-summary p:nth-of-type(1)").Text(),
 			SecondHalf: s.Find(".item-summary p:nth-of-type(2)").Text(),
+		}
+
+		date, err := time.Parse(timeFormat, s.Find("time").First().Text())
+		if err == nil {
+			show.Date = date.Format(time.DateOnly)
 		}
 
 		guests := []string{}
 		s.Find(".coast-linked-guests .linked-guest-value a").Each(func(i int, guest *goquery.Selection) {
 			guests = append(guests, guest.Text())
 		})
-		show.Guests = guests
+		show.Guests = strings.Join(guests, ", ")
 
-		shows = append(shows, show)
+		out.Shows = append(out.Shows, show)
 	})
 
-	output, err := json.MarshalIndent(shows, "", "    ")
+	output, err := json.MarshalIndent(out, "", "    ")
 	if err != nil {
 		log.Fatal(err)
 	}
